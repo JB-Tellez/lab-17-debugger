51f50b30ae4d6b4c8dc5a848defceae6cce5007b
JB Tellez <jb.tellez@gmail.com>
2018-11-06 21:30:11 -0800

    removes mongoose warnings

--- __tests__/supergoose.js
+++ __tests__/supergoose.js
@@ -21,7 +21,8 @@ export default (server) => supertest(server);
 export const startDB = async () => {
   mongoServer = new MongoMemoryServer();
   const mongoUri = await mongoServer.getConnectionString();
-  await mongoose.connect(mongoUri, (err) => {
+  const options = { useNewUrlParser: true, useCreateIndex: true };
+  await mongoose.connect(mongoUri, options, (err) => {
     if (err) console.error(err);
   });
 };
